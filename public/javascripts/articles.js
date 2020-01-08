be23d209b82e29d8c0ce273b05c0ddab1ef1ac33
SirPotatoIV <otoolej33@gmail.com>
2020-01-08 16:34:27 -0600

    file upload succesful. Error message now displayed to user on html.

--- public/javascripts/articles.js
+++ public/javascripts/articles.js
@@ -1,4 +1,7 @@
+ const uploadHelpEl = document.getElementById("uploadHelp")
+ 
  function previewFile() {
+    console.log("preview file triggered")
     const preview = document.querySelector('img');
 
     const file = document.querySelector('input[type=file]').files[0];
@@ -6,9 +9,25 @@
     const reader = new FileReader();
 
     // another load event example https://developer.mozilla.org/en-US/docs/Web/API/FileReader/load_event
-    reader.addEventListener("load", function () {
+    reader.addEventListener("load", async function () {
+        console.log("load function");
         // Updates the img tag with the base64 string that was created
         preview.src = reader.result;
+        const imgString = reader.result;
+        try{
+            const response = await axios.post('/api/Images', {image_string: imgString})
+            console.log("image string posted", response);
+            uploadHelpEl.innerText = "Success!"
+        } 
+        catch(error) {
+            const fileTooLarger = error.message.indexOf("413")
+            console.log(error, fileTooLarger)
+            if(fileTooLarger >=0){
+                uploadHelpEl.innerText = `Error occurred uploading, the image is too large`;
+            }else{
+                uploadHelpEl.innerText = `Error occurred uploading the image: ${error}`
+            }
+        }
     }, false);
 
     if (file) {
